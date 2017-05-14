3b94f201f21af7a4ecb7125a8d7af00f947f86d3
X_zzzzz <195286381@qq.com>
2017-05-14 23:06:28 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: path.resolve(__dirname, 'app/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{test: /\.js$/, use: 'babel-loader'},
+			{test: /\.css$/, use: ['style-loader', 'css-loader']}
+		]
+	},
+	plugins: [new htmlWebpackPlugin({
+		template: 'app/index.html'
+	})]
+}
